60098db2ed3d7731fd9bb6a3addf754d33b6ac55
islo123 <43340489+islo123@users.noreply.github.com>
2023-03-13 13:02:07 +0200

    animation about

--- src/component/About.js
+++ src/component/About.js
@@ -5,17 +5,25 @@ import finFlag from '../kuvat/Fin-flag.png'
 import GBFlag from '../kuvat/GB-flag.png'
 import KSFlag from '../kuvat/KS-flag.png'
 
+import { motion } from "framer-motion";
+
 export default function About() {
   return (
     <div className='about'>
         <div className='about-section'>
             <Profile/>
-            <div className='about-language'>
-                <h4 style={{marginTop: '16px'}}>Kielet</h4>
-                <p className='flag-name'><img className='language-flag' alt='Suomen lippu' src={finFlag}/> Suomi --- erinomainen</p>
-                <p className='flag-name'><img className='language-flag' alt='Iso Britannia lippu' src={GBFlag}/> Englanti --- hyvä</p>
-                <p className='flag-name'><img className='language-flag' alt='Kosovon lippu' src={KSFlag}/> Albania --- äidinkieli</p>            
-            </div>            
+            <motion.div 
+              layout
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 3 }}>
+              <div className='about-language'>
+                  <h4 style={{marginTop: '16px'}}>Kielet</h4>
+                  <p className='flag-name'><img className='language-flag' alt='Suomen lippu' src={finFlag}/> Suomi --- erinomainen</p>
+                  <p className='flag-name'><img className='language-flag' alt='Iso Britannia lippu' src={GBFlag}/> Englanti --- hyvä</p>
+                  <p className='flag-name'><img className='language-flag' alt='Kosovon lippu' src={KSFlag}/> Albania --- äidinkieli</p>            
+              </div>
+            </motion.div>        
         </div>
 
         <Projects/>
--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,37 +1,71 @@
 import React from 'react'
+import { motion } from "framer-motion";
+
 
 export default function Profile() {
   return (
     <div className='profile'>
-        <p>
-            Olen pätevä Full Stack -kehittäjä.
-            Minulla on datanomin tutkinto
-            painotuksella frontend-kehittäjä
-            Taitotalo ammattikoulusta.
-            <br/>
-            <br/>
-            Itseohjautuva työskentely on
-            minulle luontaista. Kiinnostukseni
-            ohjelmoinnin ymmärtämiseen ja
-            taittamiseen kokonaisvaltaisesti on
-            motivoinut minua kehittämään
-            taitojani myös backend puolella.
-            <br/>
-            <br/>
-            Olen päättänyt opintoni Opiframella
-            Full Stack -kehittäjäksi, opintoni
-            sisälsivät työharjoittelun, jota
-            suoritin PHZ FullStack Oy:llä.
-            <br/>
-            <br/>
-            PHZ:all olen ollut mukana monessa
-            eri projektissa, joiden myötä
-            kokemusta on karttunut erilaisista
-            tehtävistä, rooleista ja työtavoista.
-            <br/>
-            <br/>
-            Vapaa-ajallani viihdyn liikunnan, musiikin ja teknologian parissa.
-        </p>
+      <motion.div 
+          layout
+          initial={{ transform: "scale(0)" }}
+          animate={{ transform: "scale(1)" }}
+          transition={{ delay: 0.5 }}>
+
+          Olen pätevä Full Stack -kehittäjä.
+          Minulla on datanomin tutkinto
+          painotuksella frontend-kehittäjä
+          Taitotalo ammattikoulusta.
+        </motion.div>
+        <br/>
+        <br/>
+        <motion.div 
+          layout
+          initial={{ transform: "scale(0)" }}
+          animate={{ transform: "scale(1)" }}
+          transition={{ delay: 1 }}>
+
+          Itseohjautuva työskentely on
+          minulle luontaista. Kiinnostukseni
+          ohjelmoinnin ymmärtämiseen ja
+          taittamiseen kokonaisvaltaisesti on
+          motivoinut minua kehittämään
+          taitojani myös backend puolella.
+        </motion.div>
+        <br/>
+        <br/>
+        <motion.div 
+          layout
+          initial={{ transform: "scale(0)" }}
+          animate={{ transform: "scale(1)" }}
+          transition={{ delay: 1.5 }}>
+          Olen päättänyt opintoni Opiframella
+          Full Stack -kehittäjäksi, opintoni
+          sisälsivät työharjoittelun, jota
+          suoritin PHZ FullStack Oy:llä.
+        </motion.div>
+        <br/>
+        <br/>
+        <motion.div 
+          layout
+          initial={{ transform: "scale(0)" }}
+          animate={{ transform: "scale(1)" }}
+          transition={{ delay: 2 }}>
+          PHZ:all olen ollut mukana monessa
+          eri projektissa, joiden myötä
+          kokemusta on karttunut erilaisista
+          tehtävistä, rooleista ja työtavoista.
+        </motion.div>
+        <br/>
+        <br/>
+        <motion.div 
+          layout
+          initial={{ transform: "scale(0)" }}
+          animate={{ transform: "scale(1)" }}
+          transition={{ delay: 2.5 }}>
+          Vapaa-ajallani viihdyn liikunnan, musiikin ja teknologian parissa.
+        </motion.div>
+        <br/>
+        <br/>
     </div>
   )
 }
